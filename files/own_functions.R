e66e4a1b54a99acbf8977d01b770d67fe95f75b2
Eero <eero@kuusisto.de>
2019-11-03 22:48:45 +0100

    Added PDF recognition with page count

--- files/own_functions.R
+++ files/own_functions.R
@@ -1,8 +1,11 @@
 library(gdata)
 library(tools)
+library(pdftools)
 # Adds a file as a link followed by the file extension and size in kB in parenthesis.
 add_file <- function(file_name, title) {
-  #if(file.size(humanRefile_name) <= ){unit <- "MB"}else{unit <- "KB"}
-  file_ext_size <- paste("[", title, "](", file_name, ") (", file_ext(file_name), ", ", humanReadable(file.size(file_name), standard="SI"), ")", sep ="")
+  file_ext_size <- paste("[", title, "](", file_name, ") (", toupper(file_ext(file_name)), ", ",
+                         if(file_ext(file_name) == "pdf"){
+                          paste(pdf_info(file_name)[2], "Seiten, ")},
+                          humanReadable(file.size(file_name), standard="SI"), ")", sep ="")
   return(file_ext_size)
   }
