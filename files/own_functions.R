056a1ddc91f8fb315fe773b8ccd31e3c0495f76a
Eero <eero@kuusisto.de>
2019-11-03 22:50:00 +0100

    Improved documentation

--- files/own_functions.R
+++ files/own_functions.R
@@ -1,7 +1,9 @@
+# Adds a file as a link followed by the file extension, page count (if PDF) and size in kB in parenthesis:
+
 library(gdata)
 library(tools)
 library(pdftools)
-# Adds a file as a link followed by the file extension and size in kB in parenthesis.
+
 add_file <- function(file_name, title) {
   file_ext_size <- paste("[", title, "](", file_name, ") (", toupper(file_ext(file_name)), ", ",
                          if(file_ext(file_name) == "pdf"){
