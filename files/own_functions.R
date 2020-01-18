be44029304c59225c4575ececf89a1115e515ba5
Eero <eero@kuusisto.de>
2020-01-18 13:45:57 +0100

    Bug fix in add_file

--- files/own_functions.R
+++ files/own_functions.R
@@ -1,5 +1,4 @@
 # Adds a file as a link followed by the file extension, page count (if PDF) and size in kB in parenthesis:
-
 library(gdata)
 library(tools)
 library(pdftools)
@@ -7,6 +6,6 @@ library(pdftools)
 add_file <- function(file_name, title, font_size = 2) {
   paste("[", title, "](", file_name, ") <font size=", font_size, ">(", toupper(file_ext(file_name)), ", ",
         if(file_ext(file_name) == "pdf"){
-        paste(pdf_info(file_name)[2], "Seiten, ")},
+        ifelse(pdf_info(file_name)[2] == 1, paste(pdf_info(file_name)[2], "Seite, "), paste(pdf_info(file_name)[2], "Seiten, "))},
         humanReadable(file.size(file_name), standard="SI"), ")</font>", sep ="")
   }
